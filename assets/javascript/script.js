ebb6304d8515969ed4676cd3d8e42d1df9bebd78
Marshall Johnston <marshalljohnston77@gmail.com>
2018-06-20 22:05:32 -0400

    More javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,18 +1,14 @@
 
-
-
-
-
 $("#submit-search-btn").on("click", function() {
   var search = $("#site-search").val();
   var dateStart = $("#start-year-search").val();
   var dateEnd = $("#end-year-search").val();
-  var pageLimit = $("#")
-
+  var pageLimit = $("#results-num").val(); 
+console.log(search + dateStart + dateEnd + pageLimit);
   // get all form data and transfer to variable
 
   
-  "oil" //value of search input
+
   
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
@@ -24,19 +20,32 @@ url += '?' + $.param({
   'page': pageLimit
 
 });
-    $.ajax({
-    url: url,
-    method: 'GET',
-    })
-    
-    .done(function(response) {
 
-    console.log(response);
+$.ajax({
+  url: url,
+  method: 'GET',
+})
 
-    var results = response.data;
-    console.log(results);
+.done(function(response) {
+  
+  
+  for (var i = 0; i < (pageLimit*10); i++) {
+
+      console.log(response);
+      console.log(response.response.docs[i].headline.main);
+      var articleContainer  = $("<div>").addClass("article-container");
+      var printHeadline = $("<div>").html(response.response.docs[i].headline.print_headline);
+      var main = $("<div>").html(response.response.docs[i].headline.main);
+      var author = $("<div>").html(response.response.docs[i].byline.original);
+      var dateWritten = $("<div>").html(response.response.docs[i].pub_date);
+
+
+      articleContainer.append(printHeadline, main, author, dateWritten);
+      $("#overall-container").append(articleContainer);
+      };
+  
     });
-
 });
+  
 
  
